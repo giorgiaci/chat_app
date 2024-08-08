7af22373e3319276774e0dcbed3f3af4227048ab
Giorgia Ciulla <giorgiaciulla.pa@gmail.com>
2024-08-09 00:00:06 +0100

    Implemented soketio into server

--- be/src/index.mjs
+++ be/src/index.mjs
@@ -1,20 +1,57 @@
 import express from "express";
 import routes from './routes/index.mjs';
+import cors from 'cors';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 import configEnv from '../config-env.js';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 
-const url = ``;
+// Create HTTP server
+const server = createServer(app);
+
+// Initialize Socket.IO with the server
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:4200'],
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+});
+
+
+const url = `mongodb+srv://${configEnv.db_username}:${configEnv.db_password}@${configEnv.db_name}.${configEnv.db_id}.mongodb.net/${configEnv.db_Name}?retryWrites=true&w=majority&appName=${configEnv.db_Name}`;
+
 
 app.use(express.json());
+
+app.use(cookieParser())
+
+app.use(cors({
+    credentials: true,
+    origin:['http://localhost:4200']
+}));
 
 app.use(routes);
 
 const PORT = configEnv.port || 3000;
 
+
 app.get("/", (req, res)=>{
     res.status(201).send({msg: "Hello!!"});
+});
+
+// Socket.IO connection handler
+io.on('connection', (socket) => {
+    socket.on('message', (data, username) => {
+        io.emit('message', {data, username}); // Broadcast the message to all clients 
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 mongoose.connect(url)
